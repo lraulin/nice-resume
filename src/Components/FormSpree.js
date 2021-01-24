a9cbc86da939e76306cf739700ebd4606f702876
Lee Raulin <leeraulin@gmail.com>
2021-01-24 17:54:18 -0500

    functional refactor of form component

--- src/Components/FormSpree.js
+++ src/Components/FormSpree.js
@@ -1,35 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class FormSpree extends React.Component {
-  constructor(props) {
-    super(props);
-    this.submitForm = this.submitForm.bind(this);
-    this.state = {
-      status: '',
-    };
-  }
+const FormSpree = () => {
+  const [status, setStatus] = useState('');
 
-  render() {
-    const { status } = this.state;
-    return (
-      <form
-        onSubmit={this.submitForm}
-        action="https://formspree.io/f/xgepdpkg"
-        method="POST"
-      >
-        <label>Email:</label>
-        <input type="email" name="email" />
-        <label>Message:</label>
-        <textarea cols="50" rows="15" name="message" />
-        {status === 'SUCCESS' ? <p>Thanks!</p> : <button>Submit</button>}
-        {status === 'ERROR' && <p>Ooops! There was an error.</p>}
-      </form>
-    );
-  }
-
-  submitForm(ev) {
-    ev.preventDefault();
-    const form = ev.target;
+  const submitForm = (event) => {
+    event.preventDefault();
+    const form = event.target;
     const data = new FormData(form);
     const xhr = new XMLHttpRequest();
     xhr.open(form.method, form.action);
@@ -38,11 +14,28 @@ export default class FormSpree extends React.Component {
       if (xhr.readyState !== XMLHttpRequest.DONE) return;
       if (xhr.status === 200) {
         form.reset();
-        this.setState({ status: 'SUCCESS' });
+        setStatus('SUCCESS');
       } else {
-        this.setState({ status: 'ERROR' });
+        setStatus('ERROR');
       }
     };
     xhr.send(data);
-  }
-}
+  };
+
+  return (
+    <form
+      onSubmit={submitForm}
+      action="https://formspree.io/f/xgepdpkg"
+      method="POST"
+    >
+      <label>Email:</label>
+      <input type="email" name="email" />
+      <label>Message:</label>
+      <textarea cols="50" rows="15" name="message" />
+      {status === 'SUCCESS' ? <p>Thanks!</p> : <button>Submit</button>}
+      {status === 'ERROR' && <p>Ooops! There was an error.</p>}
+    </form>
+  );
+};
+
+export default FormSpree;
