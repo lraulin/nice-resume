80e83ec068076d45a84b68d52313f5b3c7ea245b
Lee Raulin <leeraulin@gmail.com>
2021-02-18 16:40:29 -0500

    Add resume for download from static assets

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import './Project.css';
 
 const Project = ({ project }) => {
   const { title, problem, solution, outcome } = project;
   return (
     <section>
       <h3>{title}</h3>
-      <h4>Problem</h4>
-      <p>{problem}</p>
-      <h4>Solution</h4>
-      <p>{solution}</p>
-      <h4>Outcome</h4>
-      <p>{outcome}</p>
+      <p>
+        <h4>Problem</h4>
+        {problem}
+      </p>
+      <p>
+        <h4>Solution</h4>
+        {solution}
+      </p>
+      <p>
+        <h4>Outcome</h4>
+        {outcome}
+      </p>
     </section>
   );
 };
